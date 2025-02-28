b4217af74132f95cdf1bd919b39f49c34ef57b69
pablo080 <pablowerner080@gmail.com>
2025-02-28 13:42:26 -0300

    massage commit

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; // Importação do HttpClientModule
-import { AppComponent } from './app.component';
-import { ClienteListComponent } from './components/cliente-list/cliente-list-component';
+import { AppComponent } from './components/app/app.component'; // Importação do componente
 
 @NgModule({
     declarations: [
-        AppComponent,
-        ClienteListComponent
+        // Remova AppComponent daqui
     ],
     imports: [
         BrowserModule,
-        HttpClientModule
+        AppComponent // Adicione aqui
     ],
     providers: [],
     bootstrap: [AppComponent]
