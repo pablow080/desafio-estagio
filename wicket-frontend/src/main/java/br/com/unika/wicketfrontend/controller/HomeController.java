fd96e0bbdee696c656fe0da94c18e66fc79a5059
pablo <pablowerner080@gmail.com>
2025-03-10 11:07:57 -0300

    massage commit

---
+++ wicket-frontend/src/main/java/br/com/unika/wicketfrontend/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.unika.wicketfrontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Nome do template ou página que você deseja renderizar
+    }
+}
